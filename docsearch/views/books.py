77446db686641957c37d8572d1e27fc1584c9468
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2019-12-10 11:03:58 -0600

    Rename template subdirectories to use plural model names

--- docsearch/views/books.py
+++ docsearch/views/books.py
@@ -6,20 +6,20 @@ from . import base as base_views
 
 class BookDetail(base_views.BaseDetailView):
     model = models.Book
-    template_name = 'docsearch/book/detail.html'
+    template_name = 'docsearch/books/detail.html'
     update_url = 'book-update'
 
 
 class BookCreate(base_views.BaseCreateView):
     model = models.Book
-    template_name = 'docsearch/book/form.html'
+    template_name = 'docsearch/books/form.html'
     fields = '__all__'
     cancel_url = 'home'  # TODO: Change this to Search
 
 
 class BookUpdate(base_views.BaseUpdateView):
     model = models.Book
-    template_name = 'docsearch/book/form.html'
+    template_name = 'docsearch/books/form.html'
     fields = '__all__'
     cancel_url = 'book-detail'
     delete_url = 'book-delete'
@@ -27,6 +27,6 @@ class BookUpdate(base_views.BaseUpdateView):
 
 class BookDelete(base_views.BaseDeleteView):
     model = models.Book
-    template_name = 'docsearch/book/confirm_delete.html'
+    template_name = 'docsearch/books/confirm_delete.html'
     cancel_url = 'book-detail'
     success_url = reverse_lazy('book-create')  # TODO: Change to search
--- docsearch/views/controlmonumentmaps.py
+++ docsearch/views/controlmonumentmaps.py
@@ -6,20 +6,20 @@ from . import base as base_views
 
 class ControlMonumentMapDetail(base_views.BaseDetailView):
     model = models.ControlMonumentMap
-    template_name = 'docsearch/controlmonumentmap/detail.html'
+    template_name = 'docsearch/controlmonumentmaps/detail.html'
     update_url = 'controlmonumentmap-update'
 
 
 class ControlMonumentMapCreate(base_views.BaseCreateView):
     model = models.ControlMonumentMap
-    template_name = 'docsearch/controlmonumentmap/form.html'
+    template_name = 'docsearch/controlmonumentmaps/form.html'
     fields = '__all__'
     cancel_url = 'home'  # TODO: Change this to Search
 
 
 class ControlMonumentMapUpdate(base_views.BaseUpdateView):
     model = models.ControlMonumentMap
-    template_name = 'docsearch/controlmonumentmap/form.html'
+    template_name = 'docsearch/controlmonumentmaps/form.html'
     fields = '__all__'
     cancel_url = 'controlmonumentmap-detail'
     delete_url = 'controlmonumentmap-delete'
@@ -27,6 +27,6 @@ class ControlMonumentMapUpdate(base_views.BaseUpdateView):
 
 class ControlMonumentMapDelete(base_views.BaseDeleteView):
     model = models.ControlMonumentMap
-    template_name = 'docsearch/controlmonumentmap/confirm_delete.html'
+    template_name = 'docsearch/controlmonumentmaps/confirm_delete.html'
     cancel_url = 'controlmonumentmap-detail'
     success_url = reverse_lazy('controlmonumentmap-create')  # TODO: Change to search
