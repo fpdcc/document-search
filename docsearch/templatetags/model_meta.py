8ec7a93baf7cb0d8abb8e6a2157d7821355ae88d
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2019-12-10 14:43:43 -0600

    Add object metadata to detail views

--- docsearch/templatetags/model_meta.py
+++ docsearch/templatetags/model_meta.py
@@ -16,3 +16,12 @@ def verbose_name(obj):
 @register.filter
 def verbose_name_plural(obj):
     return obj._meta.verbose_name_plural
+
+
+@register.filter
+def field_name(value, field):
+    """
+    Return the verbose fieldname for a given 'field' on an object or Model.
+    """
+    Model = value.model if hasattr(value, 'model') else value
+    return Model._meta.get_field(field).verbose_name
