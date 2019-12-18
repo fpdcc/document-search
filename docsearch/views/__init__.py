c8bda4387b5f828b70bd69173c833d45b5397a24
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2019-12-18 15:43:18 -0600

    Fix variable references for app_start.sh

--- docsearch/views/__init__.py
+++ docsearch/views/__init__.py
@@ -28,7 +28,7 @@ def pong(request):
     from django.http import HttpResponse
 
     try:
-        from .deployment import DEPLOYMENT_ID
+        from ..deployment import DEPLOYMENT_ID
     except ImportError as e:
         return HttpResponse('Bad deployment: {}'.format(e), status=401)
 
