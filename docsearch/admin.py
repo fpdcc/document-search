0c81bbc43cb20033de9e4aa173a103b97d1bfba7
Xavier Medrano <xaviermedrano@Xaviers-MacBook-Pro.local>
2023-08-29 10:51:31 -0400

    create notification model and add to admin

--- docsearch/admin.py
+++ docsearch/admin.py
@@ -4,3 +4,7 @@ from docsearch import models
 
 admin.site.site_header = 'Forest Preserve of Cook County Document Search Admin'
 admin.site.site_title = 'Document Search'
+
+class NotificationSubscriptionAdmin(admin.ModelAdmin):
+  list_display = ["user"]
+admin.site.register(models.NotificationSubscription, NotificationSubscriptionAdmin)
